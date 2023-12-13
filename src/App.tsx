28329ea04a4af8a3377ff9012857693733a98066
Aleksandar <radicevicsasa836@gmail.com>
2023-12-13 21:58:17 +0100

    add supabase api for getting and posting messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import { For, createSignal } from "solid-js";
+import { For, createEffect, createResource, createSignal } from "solid-js";
 import "./App.css";
+import { supabase } from "./utils/supabaseClient";
 
 interface Message {
+  id: number;
+  createdAt: string;
   message: string;
-  username: string | null;
+  username: string;
+}
+
+async function getMessages() {
+  const { data } = await supabase.from("publicMessages").select();
+  return data;
 }
 
 function App() {
-  const [messages, setMessages] = createSignal<Message[]>([
-    { message: "You got this", username: "Ljuljo" },
-    { message: "You got this", username: null },
-    { message: "You got this", username: "Marko" },
-    { message: "You got this", username: "Vidoje" },
-  ]);
+  const [allMessages] = createResource(getMessages);
+  const [message, setMessage] = createSignal<string>("");
+  const [username, setUsername] = createSignal<string>("");
+
+  async function sendMessage() {
+    const { data, error } = await supabase
+      .from("publicMessages")
+      .insert({ message: message(),  username: username() })
+      .select();
+
+    if(data){
+      alert('sjajno')
+    }else{
+      alert(error)
+    }
+  }
+
   return (
     <>
       <div class="introduction">
@@ -21,15 +40,40 @@ function App() {
           Leave him a message of support and encouragement, and remind him that
           true love is worth the wait. &#x2764;&#xFE0F;
         </p>
+        <div class="inputField">
+          <input
+            id="inputMessage"
+            value={message()}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            type="text"
+            placeholder="Your message go here..."
+          />
+          <input
+            id="inputUsername"
+            value={username()}
+            type="text"
+            placeholder="Your username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <button class="sendButton" onclick={() => sendMessage()}>
+            Send!
+          </button>
+        </div>
       </div>
       <main class="container">
         <h3>Your lovley messages:</h3>
         <div class="messagesContainer">
-          <For each={messages()}>
+          <For each={allMessages()}>
             {(message, _i) => (
               <div class="message">
                 <p>{message.message}</p>
-                <p class="username">- {message.username ?? "Anonymous"}</p>
+                <p class="username">
+                  - {message.username !== "" ? message.username : "Anonymous"}
+                </p>
               </div>
             )}
           </For>
