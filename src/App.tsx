57275f689133fe9b3c780a073adc10a55c952a26
Aleksandar <radicevicsasa836@gmail.com>
2023-12-16 00:48:42 +0100

    posting and getting messages improve, style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,71 @@
-import { For, createEffect, createResource, createSignal } from "solid-js";
+import {
+  For,
+  Show,
+  createEffect,
+  createResource,
+  createSignal,
+} from "solid-js";
 import "./App.css";
 import { supabase } from "./utils/supabaseClient";
 
 interface Message {
   id: number;
-  createdAt: string;
+  created_at: string;
   message: string;
   username: string;
 }
 
 async function getMessages() {
   const { data } = await supabase.from("publicMessages").select();
-  return data;
+  return data as Message[];
+}
+
+function NoMessages() {
+  return (
+    <div id="noMessages">
+      <h1>No messages... &#x1F622;</h1>
+    </div>
+  );
 }
 
 function App() {
-  const [allMessages] = createResource(getMessages);
+  const [allMessages, { refetch }] = createResource<Message[]>(getMessages);
+  const [messages, setMessages] = createSignal<Message[]>([]);
   const [message, setMessage] = createSignal<string>("");
   const [username, setUsername] = createSignal<string>("");
+  const [charCount, setCharCount] = createSignal(0);
 
   async function sendMessage() {
-    const { data, error } = await supabase
-      .from("publicMessages")
-      .insert({ message: message(),  username: username() })
-      .select();
+    if (message() !== "") {
+      const { error } = await supabase
+        .from("publicMessages")
+        .insert({ message: message(), username: username() })
+        .select();
 
-    if(data){
-      alert('sjajno')
+      refetch();
+
+      if (error) {
+        console.log(error);
+      }
     }else{
-      alert(error)
+      alert('You need to write something in order to post it.')
     }
   }
+
+  const updateCharCount = (e: any): void => {
+    setCharCount(e.target.value.length);
+  };
+
+  createEffect(() => {
+    let msgs = allMessages();
+    if (messages().length === 0) {
+      msgs !== undefined && msgs.length > 0
+        ? setMessages((prev) => [...prev, msgs[msgs.length - 1]])
+        : null;
+    } else {
+      msgs !== undefined ? setMessages([...msgs]) : null;
+    }
+  });
 
   return (
     <>
@@ -40,44 +75,67 @@ function App() {
           Leave him a message of support and encouragement, and remind him that
           true love is worth the wait. &#x2764;&#xFE0F;
         </p>
-        <div class="inputField">
-          <input
-            id="inputMessage"
-            value={message()}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            type="text"
-            placeholder="Your message go here..."
-          />
+        <div class="inputFields">
+          <div class="inputMessageContainer">
+            <textarea
+              id="inputMessage"
+              maxlength={200}
+              value={message()}
+              oninput={(e) => {
+                setMessage(e.target.value);
+                updateCharCount(e);
+              }}
+              placeholder="Your message goes here..."
+            />
+            <p id="charCount">{charCount()}/200</p>
+          </div>
           <input
             id="inputUsername"
             value={username()}
             type="text"
-            placeholder="Your username"
+            placeholder="Your username goes here..."
             onChange={(e) => {
               setUsername(e.target.value);
             }}
           />
-          <button class="sendButton" onclick={() => sendMessage()}>
-            Send!
+          <button
+            disabled={message()! === ""}
+            id="sendButton"
+            onclick={() => sendMessage()}
+          >
+            SEND!
           </button>
         </div>
       </div>
       <main class="container">
         <h3>Your lovley messages:</h3>
-        <div class="messagesContainer">
-          <For each={allMessages()}>
-            {(message, _i) => (
-              <div class="message">
-                <p>{message.message}</p>
-                <p class="username">
-                  - {message.username !== "" ? message.username : "Anonymous"}
-                </p>
-              </div>
-            )}
-          </For>
-        </div>
+        <Show when={messages().length > 0} fallback={<NoMessages />}>
+          <div class="messagesContainer">
+            <For each={messages()}>
+              {(message, _i) => (
+                <div class="message">
+                  <p class="createAt">
+                    {new Date(message.created_at).toLocaleString()}
+                  </p>
+                  <p
+                    style={{
+                      "align-self": "center",
+                      "max-width": "80%",
+                      "word-wrap": "break-word",
+                      "font-size": "1.2em",
+                    }}
+                  >
+                    {message.message}
+                  </p>
+                  <p class="username">
+                    By:{" "}
+                    {message.username !== "" ? message.username : "Anonymous"}
+                  </p>
+                </div>
+              )}
+            </For>
+          </div>
+        </Show>
       </main>
     </>
   );
